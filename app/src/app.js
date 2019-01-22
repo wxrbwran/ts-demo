d13a806a6f87f2ee10360333491967daa2d6a8b1
wuxiaoran <wxrbw@qq.com>
2019-01-22 18:24:53 +0800

    chore:

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Koa_1 = require("Koa");
-var app = new Koa_1.default();
+// import Koa from 'Koa';
+require("reflect-metadata"); // this shim is required
+var routing_controllers_1 = require("routing-controllers");
+var UserController_1 = require("./UserController");
+var app = routing_controllers_1.createKoaServer({
+    controllers: [UserController_1.UserController] // we specify controllers we want to use
+});
 app.listen(6666);
--- app/src/app.ts
+++ app/src/app.ts
@@ -1,5 +1,11 @@
-import Koa from 'Koa';
+// import Koa from 'Koa';
+import "reflect-metadata"; // this shim is required
+import {createKoaServer} from "routing-controllers";
+import {UserController} from "./UserController";
 
-const app = new Koa();
+
+const app = createKoaServer({
+    controllers: [UserController] // we specify controllers we want to use
+});
 
 app.listen(6666);
